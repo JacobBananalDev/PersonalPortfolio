70dea10f9959a04edb63f255b9352564f56544bc
JbanzCodingLabz <jacob.r.bananal@outlook.com>
2024-09-19 20:25:04 -0700

    PersonalPortfolio - Added cards.jsx, projectslist.js, contants.js

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,8 +2,45 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-        
+    <div className="mb-24 h-auto w-full sm:mb-0 md:h-screen">
+      <div className="mx-auto flex w-[90%] flex-col justify-between rounded-lg bg-transparent p-4 shadow-lg md:flex-row md:items-center">
+        <div className="flex w-full flex-col md:w-[50%]">
+          <p className="pb-2 text-2xl font-semibold tracking-wide text-gray-900">
+            About Me
+          </p>
+          <p className="font-poppins text-sm lg:text-base"></p>
+          <div className="mt-8">
+            {/* <Marquee
+              gradient={false}
+              speed={150}
+              pauseOnClick={true}
+              delay={0}
+              play={true}
+              direction="right"
+            >
+              {skillsData.map((skill, id) => (
+                <div
+                  className="ml-4 flex h-24 w-24 flex-col items-center justify-center gap-2"
+                  key={id}
+                >
+                  <img
+                    className="h-[50px] w-[60px] bg-contain bg-no-repeat"
+                    src={skillsImage(skill)}
+                    alt={skill}
+                  />
+                  <p>{skill}</p>
+                </div>
+              ))}
+            </Marquee> */}
+          </div>
+        </div>
+
+        {/* <img
+          className="max-w[500px] mt-4 bg-cover bg-center bg-no-repeat md:w-[350px] lg:mt-0 lg:h-[350px] lg:w-[550px]"
+          src={AboutImage}
+          alt=""
+        /> */}
+      </div>
     </div>
   )
 }
